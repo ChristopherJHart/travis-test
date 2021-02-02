67e8c2f56004d6458a9166d903f6171c4d0ac947
Christopher Hart <chart2@cisco.com>
2021-02-02 16:47:26 -0500

    Add simple Python script

---
+++ script.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def main():
+    print("Hello world!")
+    for x in range(0, 10):
+        print(f"{x} -> {x * x}")
+    print("All done!")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
