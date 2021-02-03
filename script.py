ae17e9471852e3acfc23199a51e9c60444a1186b
Christopher Hart <chart2@cisco.com>
2021-02-03 13:08:20 -0500

    Execute script in CI/CD, which involves making a request

--- script.py
+++ script.py
@@ -2,10 +2,14 @@ import sys
 import requests
 
 
+def make_request():
+    return requests.get("https://google.com")
+
 def main():
     print("Hello world!")
     for x in range(0, 10):
         print(f"{x} -> {x * x}")
+    make_request()
     print("All done!")
 
 
